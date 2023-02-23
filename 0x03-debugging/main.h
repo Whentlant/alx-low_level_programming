357dc94a87521893003f81d7b7bbd982cc49d678
Whentlant <whentlantfedha@gmail.com>
2023-02-23 12:21:38 -0800

    number signs

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,15 +1,13 @@
-#include "main.h"
-/**
-*main- tests function that prints id integer is positive or negative
-*Return: 0
-*/
+#ifndef MAIN_H
+#define MAIN_H
 
-int main(void)
-{
-        int i;
+#include <stdio.h>
 
-        i=0;
-        positive_or_negative(i);
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
-        return (0);
+
+#endif
 }
