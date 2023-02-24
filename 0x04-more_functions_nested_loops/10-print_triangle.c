28d00ab2c07cde48b305bb732b81e61b4b2420bb
Whentlant <whentlantfedha@gmail.com>
2023-02-24 11:23:15 -0800

    prints triangle witha hash character

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,24 +1,39 @@
 #include "main.h"
+
 /**
- * print_triangle- prints triangle.
- * @size: size of the triangle
- *
+ * print_triangle - prints a triangle
+ * @size: variable
+ * Return: 0
  */
+
 void print_triangle(int size)
 {
-	int i, j;
+	int a, b, c;
 
 	if (size <= 0)
 	{
 		_putchar('\n');
-		return;
 	}
-	for (i = 1; i <= size; i++)
+	else
 	{
-		for (j = 1; j <= i; j++)
+		a = 1;
+		while (a <= size)
 		{
-			_putchar('#');
+			b = size - a;
+			while (b >= 1)
+			{
+				_putchar(' ');
+				b--;
+			}
+			c = 1;
+			while (c <= a)
+			{
+				_putchar('#');
+				c++;
+			}
+			_putchar('\n');
+			a++;
 		}
-		_putchar('\n');
 	}
 }
+
