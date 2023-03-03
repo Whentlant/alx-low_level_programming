4425140cb13d9bf37f07a3f90c2026b43345b3b6
Whentlant <whentlantfedha@gmail.com>
2023-03-03 06:49:25 -0800

    a function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,41 +1,36 @@
 #include "main.h"
-#include <stdio.h>
+
 
 /**
- * cap_string - capitalizes most of the words in a string.
- * @s: analized string.
- *
- * Return: String with all words capitalized.
+ * cap_string - capitalizes all words of a string
+ * @s: string to be capitalized
+ * Return: pointer to the capitalized string
  */
 
 char *cap_string(char *s)
-
 {
-	int i, j;
-	int a[] = {22, 7, 10, 44, 69, 76, 23, 87, 14, 54, 19, 23, 35};
+	int i = 0;
 
-	i = 0;
-	while (*(s + i) != '\0')
+	while (s[i])
 	{
-		if (*(s + i) >= 'a' && *(s + i) <= 'z')
-		{
-			if (i == 0)
-			{
-				*(s + i) = *(s + i) - 32;
-			}
-			else
-			{
-				for (j = 0; j <= 12; j++)
-				{
-					if (a[j] == *(s + i - 1))
-					{
-						*(s + i) = *(s + i) - 32;
-					}
-				}
-			}
-		}
-	i++;
+		while (!(s[i] >= 'a' && s[i] <= 'z'))
+			i++;
+		if (s[i - 1] == ' ' ||
+				s[i - 1] == '\t' ||
+				s[i - 1] == '\n' ||
+				s[i - 1] == ',' ||
+				s[i - 1] == ';' ||
+				s[i - 1] == '.' ||
+				s[i - 1] == '!' ||
+				s[i - 1] == '?' ||
+				s[i - 1] == '"' ||
+				s[i - 1] == '(' ||
+				s[i - 1] == ')' ||
+				s[i - 1] == '{' ||
+				s[i - 1] == '}' ||
+				i == 0)
+			s[i] -= 32;
+		i++;
 	}
 	return (s);
 }
-
